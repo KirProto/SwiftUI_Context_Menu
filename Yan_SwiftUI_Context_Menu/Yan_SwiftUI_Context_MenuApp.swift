8d0e6ac6a471950a4cf96e46bfa3afe39daab9ae
Kir Proto Q <kir.proto@ya.ru>
2022-02-18 10:22:35 +0500

    Initial Commit

---
+++ Yan_SwiftUI_Context_Menu/Yan_SwiftUI_Context_MenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  Yan_SwiftUI_Context_MenuApp.swift
+//  Yan_SwiftUI_Context_Menu
+//
+//  Created by KIR Q on 2022.02.18.
+//
+
+import SwiftUI
+
+@main
+struct Yan_SwiftUI_Context_MenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
